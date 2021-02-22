ba8e95f3aa294541ceeb6001714211a302d6c8e2
陈银龙 <15995725869@163.com>
2021-02-22 18:19:58 +0800

    修改请求配置

--- src/api/users.js
+++ src/api/users.js
@@ -2,17 +2,17 @@
  * api接口统一管理
  */
 // import request from '@/utils/request'
-import {get, post, postQs, postjson, getNoloading} from './request'
+import { get, post, postQs, postjson, getNoloading } from './request'
 import baseUrl from './baseURL'
+import request from '@/utils/request'
 
 // 分页获取用户列表
-export function getUsers(data) {
-  return get(baseUrl.Api+'/api/users', data)
-  // return request({
-  //   url: baseUrl.Api+'/api/users',
-  //   method: 'get',
-  //   data
-  // })
+export function userList(data) {
+	return request({
+		url: '/users/',
+		method: 'get',
+		data
+	})
 }
 
 
